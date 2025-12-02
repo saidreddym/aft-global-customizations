108419313af7a70e17bd2699aa313d842f5d46c7
saidreddym <saidireddym@gmail.com>
2025-12-02 19:27:40 +0530

    aft-global

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,12 @@ resource "aws_s3_account_public_access_block" "block_all_public" {
   ignore_public_acls  = true
   restrict_public_buckets  = true
 }
+
+resource "aws_s3_bucket" "this1" {
+  bucket = "my-example-bucket-12390"
+
+  tags = {
+    Name        = "my-example-bucket-qa9338"
+    Environment = "dev"
+  }
+}
